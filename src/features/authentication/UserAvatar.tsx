b844950895b98530986ae5a87d59bcd7d30a6f8b
felipe <profissionalf.andre@gmail.com>
2024-11-22 15:57:56 -0300

    chore: add personalized header with correct icons

---
+++ src/features/authentication/UserAvatar.tsx
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+import { useUser } from '../../hooks/auth/useUser';
+
+const StyledUserAvatar = styled.div`
+  display: flex;
+  gap: 1.2rem;
+  align-items: center;
+  font-weight: 500;
+  font-size: 1.4rem;
+
+  color: var(--color-grey-600);
+`;
+
+const Avatar = styled.img`
+  display: block;
+  width: 4rem;
+  width: 3.6rem;
+  aspect-ratio: 1;
+  object-fit: cover;
+  object-position: center;
+  border-radius: 50%;
+  outline: 2px solid var(--color-grey-100);
+`;
+
+export default function UserAvatar() {
+  const { user } = useUser();
+  const { fullName = 'Guest', avatar = 'default-user.jpg' } =
+    user?.user_metadata || {};
+
+  return (
+    <StyledUserAvatar>
+      <Avatar src={avatar} alt={`Avatar of ${fullName}`} />
+      <span>{fullName}</span>
+    </StyledUserAvatar>
+  );
+}
